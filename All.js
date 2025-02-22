5d99a81bcf07cef0e315e15de898733c124acb38
Shabhaizafar <zafarshabhai03@gmail.com>
2025-02-22 11:46:39 +0530

    chages added

--- All.js
+++ All.js
@@ -1,36 +1,36 @@
 const body = document.body.innerHTML;
 const btn_menu = document.querySelector('.menu-btn');
 
-document.body.onload = function(){
-    document.body.innerHTML = "";
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET","loader.txt");
+// document.body.onload = function () {
+//     document.body.innerHTML = "";
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.open("GET", "loader.txt");
 
-    xhttp.onload = function (data) {
-        document.body.innerHTML = data.target.response;
-    }
-    xhttp.send();
-    setTimeout(() => {
-            document.querySelector('.box1').style.background = "white";
-        }, 250);
-        setTimeout(() => {
-            document.querySelector('.box2').style.background = "white";
-        }, 500);
-        setTimeout(() => {
-            document.querySelector('.box3').style.background = "white";
-        }, 750);
-        setTimeout(() => {
-            document.querySelector('.box4').style.background = "white";
-        }, 1000);
-    setTimeout(() => {
-        document.body.innerHTML = body;
-    }, 2000);
-}
-document.body.addEventListener('click',function(event){
-    if(event.target.innerText == 'menu'){
+//     xhttp.onload = function (data) {
+//         document.body.innerHTML = data.target.response;
+//     }
+//     xhttp.send();
+//     setTimeout(() => {
+//         document.querySelector('.box1').style.background = "white";
+//     }, 250);
+//     setTimeout(() => {
+//         document.querySelector('.box2').style.background = "white";
+//     }, 500);
+//     setTimeout(() => {
+//         document.querySelector('.box3').style.background = "white";
+//     }, 750);
+//     setTimeout(() => {
+//         document.querySelector('.box4').style.background = "white";
+//     }, 1000);
+//     setTimeout(() => {
+//         document.body.innerHTML = body;
+//     }, 2000);
+// }
+document.body.addEventListener('click', function (event) {
+    if (event.target.innerText == 'menu') {
         document.querySelector('.side-menu').style.display = "grid";
     }
-    if(event.target.innerText == 'close'){
+    if (event.target.innerText == 'close') {
         document.querySelector('.side-menu').style.display = "none";
     }
 });
@@ -48,9 +48,21 @@ document.body.addEventListener('click',function(event){
 // -----------------------
 
 
-function All(){
+function All() {
     document.querySelector('.past-c').style.left = "0vw";
-    document.querySelector('.past-c').style.transition= 'all 2s';
+    document.querySelector('.past-c').style.transition = 'all 2s';
 }
 
 
+
+const ul = document.querySelector('.past-ul');
+ul.addEventListener('mouseover',function(event){
+    if(event.target.tagName=='A' && !event.target.parentElement.classList.contains('active')){
+        event.target.parentElement.classList.add('active');
+    }
+});
+ul.addEventListener('mouseout',function(event){
+    if(event.target.tagName=='A' && event.target.parentElement.classList.contains('active')){
+        event.target.parentElement.classList.remove('active');
+    }
+});
